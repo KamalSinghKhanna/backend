dc77cec487fd790a880ada2e8b187fea3454c201
KamalSinghKhanna <kamalsinghkhanna2706@gmail.com>
2022-07-06 10:26:55 +0530

    move

--- app.js
+++ app.js
@@ -1,16 +1,31 @@
-const http = require('http');
+const { response } = require('express');
+const express = require('express')
 
-function handleRequest(request, response) {
-    if (request.url === '/currenttime') {
-        response.statusCode = 200;
-        response.end('<h1>'+ new Date().toISOString() + '</h1>');
-    } else if(request.url === '/') {
-        response.statusCode = 200;
-        response.end('<h1>Hello I am learning backend !</h1>');
-    }
+const app = express();
+
+
+app.get('/currenttime', function(req, res) {
+    res.send('<h1>'+ new Date().toISOString() + '</h1>');
+}) //localhost:3000/currenttime
+
+app.get('/', function(req, res) {
+    res.send('<h1>Hello I am learning backend with node js!</h1>');
+}); //localhost:3000/
+
+app.listen(3000)
+
+
+// function handleRequest(request, response) {
+//     if (request.url === '/currenttime') {
+//         response.statusCode = 200;
+//         response.end('<h1>'+ new Date().toISOString() + '</h1>');
+//     } else if(request.url === '/') {
+//         response.statusCode = 200;
+//         response.end('<h1>Hello I am learning backend !</h1>');
+//     }
    
-}
+// }
 
-const server = http.createServer(handleRequest);
+// const server = http.createServer(handleRequest);
 
-server.listen(3000);
+// server.listen(3000);
